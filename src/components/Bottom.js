ac56bacdaba11305ea96d1c0dd56a28c786f454e
JoyShaheb <61899866+JoyShaheb@users.noreply.github.com>
2020-09-22 23:30:35 +0600

    change-3 Completed the bottom.js component next Change includes styling the entire Website

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -7,30 +7,42 @@ const Bottom = () => {
           <img src="" alt="" />
         </div>
         <div className="card-info">
-          <p className="card-name"></p>
-          <p className="card-position"></p>
+          <p class="card-name">Colton Smith</p>
+          <p class="card-position">Verified Buyer</p>
         </div>
-        <div className="card-bottom"></div>
+        <div className="card-bottom">
+          "We needed the same printed design as the one we had ordered a week
+          prior. Not only did they find the original order, but we also received
+          it in time. Excellent!"
+        </div>
       </div>
       <div className="card-2">
         <div className="card-top">
           <img src="" alt="" />
         </div>
         <div className="card-info">
-          <p className="card-name"></p>
-          <p className="card-position"></p>
+          <p class="card-name">Irene Roberts</p>
+          <p class="card-position">Verified Buyer</p>
         </div>
-        <div className="card-bottom"></div>
+        <div className="card-bottom">
+          "Customer service is always excellent and very quick turn around.
+          Completely delighted with the simplicity of the purchase and the speed
+          of delivery."
+        </div>
       </div>
       <div className="card-3">
         <div className="card-top">
           <img src="" alt="" />
         </div>
         <div className="card-info">
-          <p className="card-name"></p>
-          <p className="card-position"></p>
+          <p class="card-name">Anne Wallace</p>
+          <p class="card-position">Verified Buyer</p>
         </div>
-        <div className="card-bottom"></div>
+        <div className="card-bottom">
+          "Put an order with this company and can only praise them for the very
+          high standard. Will definitely use them again and recommend them to
+          everyone!"
+        </div>
       </div>
     </div>
   );
