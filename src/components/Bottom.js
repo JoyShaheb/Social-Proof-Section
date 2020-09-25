e7483c0cacc98faa2b0d3c6ccb514f4daf3307fc
JoyShaheb <61899866+JoyShaheb@users.noreply.github.com>
2020-09-25 23:07:50 +0600

    Change-6 Done with styling the Bottom Component.

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -5,43 +5,51 @@ import irene from "../../public/images/image-irene.jpg";
 const Bottom = () => {
   return (
     <div className="bottom">
-      <div className="card-1">
-        <div className="card-top">
-          <img src={colton} alt="" />
+      <div className="CARD-1">
+        <div className="card-block">
+          <div className="card-img">
+            <img src={colton} alt="" />
+          </div>
+          <div className="card-info">
+            <p className="card-name">Colton Smith</p>
+            <p className="card-status">Verified Buyer</p>
+          </div>
         </div>
-        <div className="card-info">
-          <p className="card-name">Colton Smith</p>
-          <p className="card-position">Verified Buyer</p>
-        </div>
-        <div className="card-bottom">
+        <div className="card-text">
           "We needed the same printed design as the one we had ordered a week
           prior. Not only did they find the original order, but we also received
           it in time. Excellent!"
         </div>
       </div>
-      <div className="card-2">
-        <div className="card-top">
-          <img src={irene} alt="" />
+
+      <div className="CARD-2">
+        <div className="card-block">
+          <div className="card-img">
+            <img src={irene} alt="" />
+          </div>
+          <div className="card-info">
+            <p className="card-name">Irene Roberts</p>
+            <p className="card-status">Verified Buyer</p>
+          </div>
         </div>
-        <div className="card-info">
-          <p className="card-name">Irene Roberts</p>
-          <p className="card-position">Verified Buyer</p>
-        </div>
-        <div className="card-bottom">
+        <div className="card-text">
           "Customer service is always excellent and very quick turn around.
           Completely delighted with the simplicity of the purchase and the speed
           of delivery."
         </div>
       </div>
-      <div className="card-3">
-        <div className="card-top">
-          <img src={anne} alt="" />
+
+      <div className="CARD-3">
+        <div className="card-block">
+          <div className="card-img">
+            <img src={anne} alt="" />
+          </div>
+          <div className="card-info">
+            <p className="card-name">Anne Wallace</p>
+            <p className="card-status">Verified Buyer</p>
+          </div>
         </div>
-        <div className="card-info">
-          <p className="card-name">Anne Wallace</p>
-          <p className="card-position">Verified Buyer</p>
-        </div>
-        <div className="card-bottom">
+        <div className="card-text">
           "Put an order with this company and can only praise them for the very
           high standard. Will definitely use them again and recommend them to
           everyone!"
